a19658ef7ca3593d6616a3a2e43d5dd53cab4804
Agence312 <ia@agence312.fr>
2025-08-20 16:23:20 +0200

    Delete src directory

--- src/components/DragonSprite.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function DragonSprite({ stage }){
-  return <div style={{fontSize:'80px'}}>{stage==='egg'?'🥚': stage==='hatchling'?'🐣': stage==='juvenile'?'🐲': '🐉'}</div>
-}
